511c7d7bea04a7b33db24659966f5d799f29ee1a
Said Valencia Castrillo <savcsaid@gmail.com>
2024-10-02 15:22:57 -0500

    tasks_manager app first version

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'task_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [BASE_DIR / 'templates'],        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
