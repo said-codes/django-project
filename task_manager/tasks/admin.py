7821ae7ec1bcd07b96d0ed1a7c7f004f458fb2b3
Said Valencia Castrillo <savcsaid@gmail.com>
2024-10-02 16:54:17 -0500

    adding date and hours of create and update fields

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,3 +1,11 @@
+# admin.py
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')  # Campos a mostrar en la lista
+    search_fields = ('title',)  # Campos por los que se puede buscar
+    ordering = ('created_at',)  # Ordenar por fecha de creación
+
+
